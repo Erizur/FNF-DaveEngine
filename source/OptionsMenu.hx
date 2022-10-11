e9016ea13ba5c99820f36948b35b3d015ff00c6b
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2022-10-11 13:04:29 -0500

    HUH

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -63,7 +63,7 @@ class OptionsMenu extends MusicBeatState
 			+ "\n" + (FlxG.save.data.disableFps ? LanguageManager.getTextString('option_enable_fps') : LanguageManager.getTextString('option_disable_fps'))
 			+ "\n" + (CompatTool.save.data.compatMode ? LanguageManager.getTextString('option_disable_compat') : LanguageManager.getTextString('option_enable_compat'))
 			);
-
+			
 		grpControls = new FlxTypedGroup<Alphabet>();
 		add(grpControls);
 
@@ -157,11 +157,7 @@ class OptionsMenu extends MusicBeatState
 					updateGroupControls(FlxG.save.data.disableFps ? LanguageManager.getTextString('option_enable_fps') : LanguageManager.getTextString('option_disable_fps'), 8, 'Vertical');
 				case 9:
 					CompatTool.save.data.compatMode = !CompatTool.save.data.compatMode;
-<<<<<<< Updated upstream
 					updateGroupControls(CompatTool.save.data.compatMode ? LanguageManager.getTextString('option_disable_compat') : LanguageManager.getTextString('option_enable_compat'), 10, 'Vertical');
-=======
-					updateGroupControls(CompatTool.save.data.compatMode ? LanguageManager.getTextString('option_enable_compat') : LanguageManager.getTextString('option_disable_compat'), 9, 'Vertical');
->>>>>>> Stashed changes
 			}
 		}
 	}
