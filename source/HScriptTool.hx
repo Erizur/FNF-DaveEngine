aadfbd45e6eb87d119fe99a96aaddaa42e64bff5
AM_Erizur <50849821+Erizur@users.noreply.github.com>
2022-10-12 19:06:02 -0500

    Update HScriptTool.hx

--- source/HScriptTool.hx
+++ source/HScriptTool.hx
@@ -56,12 +56,87 @@ class HScriptTool implements IFlxDestroyable
     }
 
     public function executeFunc(funcName:String, ?args:Array<Any>):Dynamic {
+        var ret = _executeFunc(funcName, args);
+        return ret;
+    }
+
+    public function _executeFunc(funcName:String, ?args:Array<Any>):Dynamic {
         return null;
     }
+
+    public function setVariable(name:String, val:Dynamic) {}
+    public function getVariable(name:String):Dynamic {return null;}
+
+    public function trace(text:String, error:Bool = false) {
+        trace(text);
+    }
+
+    public function loadFile() {}
+    public function destroy() {}
 
 }
 
 class Script extends HScriptTool
 {
+    public var hscript:Interp;
+    public function new() {
+        hscript = new Interp();
+        hscript.errorHandler = function(e) {
+            this.trace('$e', true);
+        };
+        super();
+    }
 
+    public override function _executeFunc(funcName:String, ?args:Array<Any>):Dynamic {
+        super._executeFunc(funcName, args);
+        if (hscript == null)
+            return null;
+		if (hscript.variables.exists(funcName)) {
+            var f = hscript.variables.get(funcName);
+            if (Reflect.isFunction(f)) {
+                if (args == null || args.length < 1)
+                    return f();
+                else
+                    return Reflect.callMethod(null, f, args);
+            }
+		}
+        return null;
+    }
+
+    public override function loadFile() {
+        super.loadFile();
+        if (filePath == null || filePath.trim() == "") return;
+        try {
+            hscript.execute();
+        } catch(e) {
+            this.trace('${e.message}', true);
+        }
+    }
+    public function bruh() {
+        super.loadFile();
+    }
+
+    public override function trace(text:String, error:Bool = false) {
+        var posInfo = hscript.posInfos();
+
+        var lineNumber = Std.string(posInfo.lineNumber);
+        var methodName = posInfo.methodName;
+        var className = posInfo.className;
+    }
+
+    public override function setVariable(name:String, val:Dynamic) {
+        hscript.variables.set(name, val);
+        @:privateAccess
+        hscript.locals.set(name, {r: val, depth: 0});
+    }
+
+    public override function getVariable(name:String):Dynamic {
+        if (@:privateAccess hscript.locals.exists(name) && @:privateAccess hscript.locals[name] != null) {
+            @:privateAccess
+            return hscript.locals.get(name).r;
+        } else if (hscript.variables.exists(name))
+            return hscript.variables.get(name);
+
+        return null;
+    }
 }
