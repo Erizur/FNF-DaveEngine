8c0ed9c942de7233171f6df1bebfd29da65c1774
MemeHovy <124418090+MemeHovy@users.noreply.github.com>
2023-04-12 15:37:43 -0400

    found leftover code in here too

--- source/StrumNote.hx
+++ source/StrumNote.hx
@@ -18,48 +18,12 @@ class StrumNote extends FlxSprite
 		// get the frames and stuff
 		switch (type)
 		{
-			case '3D':
-				frames = Paths.getSparrowAtlas('notes/NOTE_assets_3D');
-			case 'top10awesome':
-				frames = Paths.getSparrowAtlas('notes/OMGtop10awesomehi');
-			case 'gh':
-				frames = Paths.getSparrowAtlas('notes/NOTEGH_assets');
 			default:
 				frames = Paths.getSparrowAtlas('notes/NOTE_assets');
 		}
 		// actually load in the animation
 		switch (type)
 		{
-			case 'gh':
-				animation.addByPrefix('green', 'A Strum');
-				animation.addByPrefix('red', 'B Strum');
-				animation.addByPrefix('yellow', 'C Strum');
-				animation.addByPrefix('blue', 'D Strum');
-				animation.addByPrefix('orange', 'E Strum');
-
-				switch (Math.abs(strumID))
-				{
-					case 0:
-						animation.addByPrefix('static', 'A Strum');
-						animation.addByPrefix('pressed', 'A Press', 24, false);
-						animation.addByPrefix('confirm', 'A Confirm', 24, false);
-					case 1:
-						animation.addByPrefix('static', 'B Strum');
-						animation.addByPrefix('pressed', 'B Press', 24, false);
-						animation.addByPrefix('confirm', 'B Confirm', 24, false);
-					case 2:
-						animation.addByPrefix('static', 'C Strum');
-						animation.addByPrefix('pressed', 'C Press', 24, false);
-						animation.addByPrefix('confirm', 'C Confirm', 24, false);
-					case 3:
-						animation.addByPrefix('static', 'D Strum');
-						animation.addByPrefix('pressed', 'D Press', 24, false);
-						animation.addByPrefix('confirm', 'D Confirm', 24, false);
-					case 4:
-						animation.addByPrefix('static', 'E Strum');
-						animation.addByPrefix('pressed', 'E Press', 24, false);
-						animation.addByPrefix('confirm', 'E Confirm', 24, false);
-				}
 			default:
 				animation.addByPrefix('green', 'arrowUP');
 				animation.addByPrefix('blue', 'arrowDOWN');
@@ -87,7 +51,7 @@ class StrumNote extends FlxSprite
 		}
 		animation.play('static');
 
-		antialiasing = type != '3D';
+		antialiasing = true;
 
 		setGraphicSize(Std.int(width * 0.7));
 		updateHitbox();
